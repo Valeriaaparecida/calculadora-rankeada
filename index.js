3b4fcddaeb33a9bb460766be5437dcbf4b2e4be5
Valéria <valeria777valeria@hotmail.com>
2024-11-08 14:32:11 -0300

    feat: adiciona script inicial do projeto

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function calcularNivel(vitorias, derrotas) {
+   
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = '';
+
+
+    if (vitorias < 10) {
+        nivel = 'Ferro';
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = 'Prata';
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = 'Lendário';
+    } else if (vitorias >= 101) {
+        nivel = 'Imortal';
+    }
+
+  
+    return { saldoVitorias, nivel };
+}
+
+
+let vitorias = 55;
+let derrotas = 30;
+
+
+let resultado = calcularNivel(vitorias, derrotas);
+
+
+console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} vitórias está no nível de ${resultado.nivel}.`);
